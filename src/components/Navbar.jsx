2c5819b6742b0bbcb6d94a7dad750deb337f6dfb
nomercykim <id6word@gmail.com>
2024-05-21 16:39:27 +0900

    0521

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 export default function Navbar() {
+    const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
     return (
-        <div>
-            <Link to='/'>Home</Link>
-            <Link to='/profile'>Profile</Link>
-            <Link to='/natural'>자연스러움</Link>
-            <Link to='/therapy'>자연스러움을 경험하다</Link>
-            <Link to='/wednesday'>수요일, CHO를 만나는 날</Link>
-            <Link to='/rd'>교육연구</Link>
-            <Link to='/contact'>자연으로 돌아가는 곳, 바른한의원</Link>
-        </div>
+        <nav className="bg-gray-800 p-4 flex justify-center space-x-6 text-lg">
+            <Link to='/' className="text-white hover:text-gray-300">Home</Link>
+            <Link 
+                to='/profile'
+                className="text-white hover:text-gray-300"
+            >
+                    Profile
+            </Link>
+            <Link
+                to='/natural'
+                className="text-white hover:text-gray-300"
+                onMouseEnter={()=> setIsDropdownOpen(true)}
+                onmouseleave={()=> setIsDropdownOpen(false)}
+            >
+                자연스러움{isDropdownOpen && (
+                    <div className="absolute mt-2 w-48 bg-white shadow-lg rounded-md"> 
+
+                    </div>
+                )}
+            </Link>
+            <Link 
+                to='/therapy'
+                className="text-white hover:text-gray-300"
+            >
+                    자연스러움을 경험하다
+            </Link>
+            <Link
+                to='/wednesday'
+                className="text-white hover:text-gray-300"
+            >
+                    수요일, CHO를 만나는 날
+            </Link>
+            <Link
+                to='/rd'
+                className="text-white hover:text-gray-300"
+            >
+                    교육연구
+            </Link>
+            <Link
+                to='/contact'
+                className="text-white hover:text-gray-300"
+            >
+                    자연으로 돌아가는 곳, 바른한의원
+            </Link>
+        </nav>
     );
 }
 
