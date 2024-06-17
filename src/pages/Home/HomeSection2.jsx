87c6789f181c756ba7341299ec0ea5b30ac3032d
nomercykim <id6word@gmail.com>
2024-06-17 17:54:36 +0900

    0617_1

--- src/pages/Home/HomeSection2.jsx
+++ src/pages/Home/HomeSection2.jsx
@@ -4,16 +4,36 @@ export default function HomeSection2({ sectionRef, isScrolled }) {
 
     return (
         <section 
-            ref={sectionRef} 
-            className={`opacity-0 transition-opacity duration-1000 ease-in-out 
-            whitespace-pre-line bg-gray-300 ${isScrolled ? 'fade-in' : ''} relative z-10 mt-0`}
+            ref={sectionRef} className={`
+                opacity-0
+                transition-opacity
+                duration-1000 ease-in-out 
+                whitespace-pre-line 
+                bg-gray-300 ${isScrolled ? 'fade-in' : ''} 
+                relative z-10`}
         >
-            <div className="flex flex-col justify-center h-screen">
-                <div className="absolute top-40 left-40 p-4 text-white text-6xl font-SUITERegular"style={{ lineHeight: '1.4' }}>
+            <div className="h-screen">
+                <div className="
+                    absolute
+                    top-[20%] left-[10%]
+                    text-2xl text-white 
+                    sm:top-[20%] sm:left-[10%] sm:text-3xl 
+                    md:top-[20%] md:left-[13%] md:text-4xl 
+                    lg:top-[20%] lg:left-[15%] lg:text-6xl
+                    font-SUITERegular" style={{ lineHeight: '1.4' }}
+                >
                         자연스러움을<br/> 잃으면<br/> 아픕니다.
                 </div>
                 <div className="pl-144">
-                    <div className=" text-left text-base leading-relaxed"style={{ lineHeight: '1.8' }}>
+                    <div className=" 
+                        absolute
+                        top-[33%] left-[10%]
+                        text-xs leading-relaxed
+                        sm:top-[36%] sm:left-[10%] sm:text-xs
+                        md:top-[22%] md:left-[40%] md:text-sm 
+                        lg:top-[23%] lg:left-[45%] lg:text-base
+                        " style={{ lineHeight: '1.8' }}
+                    >
                         {`자세와 움직임이 무너지면,
                         우리 몸이 밸런스를 잡지 못하면
                         4가지 자연스러운 순환이 무너집니다.
@@ -23,10 +43,26 @@ export default function HomeSection2({ sectionRef, isScrolled }) {
         
                         `}
                     </div>
-                    <div className="text-white text-left text-xl font-bold leading-relaxed"style={{ lineHeight: '1.8' }}>
+                    <div className="
+                        absolute
+                        top-[48%] left-[10%]
+                        text-xl text-white leading-relaxed
+                        sm:top-[52%] sm:left-[10%] sm:text-xl
+                        md:top-[40%] md:left-[40%] md:text-xl 
+                        lg:top-[45%] lg:left-[45%] lg:text-2xl
+                        font-SUITERegular"
+                    >
                         {`아주 조금만`}
                     </div>
-                    <div className="text-left text-base leading-relaxed"style={{ lineHeight: '1.8' }}>
+                    <div className="
+                        absolute
+                        top-[51%] left-[10%]
+                        text-xs leading-relaxed
+                        sm:top-[56%] sm:left-[10%] sm:text-xs
+                        md:top-[43%] md:left-[40%] md:text-sm
+                        lg:top-[49%] lg:left-[45%] lg:text-base
+                        " style={{ lineHeight: '1.8' }}
+                    >
                         {`
                         우리 몸의 자세를 바르게 움직일 수 있도록 도와주면,
                         우리 몸이 스스로 균형을 잡도록 도와준다면,
@@ -35,10 +71,19 @@ export default function HomeSection2({ sectionRef, isScrolled }) {
                         우리 몸이 자연의 섭리에 가까워 진다면,
                         `}
                     </div>
-                    <div className="text-left text-xl leading-relaxed" style={{ lineHeight: '1.8' }}>
+                    <div className="
+                        absolute
+                        top-[63%] left-[10%]
+                        text-xl text-white leading-relaxed 
+                        sm:top-[68%] sm:left-[10%] sm:text-xl
+                        md:top-[57%] md:left-[40%] md:text-xl
+                        lg:top-[63%] lg:left-[45%] lg:text-2xl
+                        font-SUITERegular" style={{ lineHeight: '1.8' }}
+                    >
                         {`
-                        우리의 몸은, 다시 자연으로 돌아갑니다.
-                        우리의 몸은, 늘 자연으로 돌아갈 준비가 되어 있습니다.`}
+                        우리의 몸은 다시 자연으로 돌아갑니다.
+                        우리의 몸은 늘 자연으로 돌아갈 준비가 
+                        되어 있습니다.`}
                     </div>
                 </div>
             </div>
--- src/pages/Home/HomeSection3.jsx
+++ src/pages/Home/HomeSection3.jsx
@@ -2,35 +2,65 @@ import React from 'react';
 
 export default function HomeSection3({ sectionRef, isScrolled }) {
     return (
-        <section ref={sectionRef} className={`opacity-0 transition-opacity duration-1000 ease-in-out whitespace-pre-line bg-gray-300 ${isScrolled ? 'fade-in' : ''} relative z-10 mt-0`}>
-        <div className="flex flex-col justify-center h-screen">
-            <div className="absolute top-40 left-40 p-4 text-white text-6xl font-SUITERegular"style={{ lineHeight: '1.4' }}>
-                근골격계의<br/>단 하나의 목적, <br/>
+        <section ref={sectionRef} className={`
+            opacity-0 
+            transition-opacity 
+            duration-1000 
+            ease-in-out 
+            whitespace-pre-line 
+            bg-gray-300 ${isScrolled ? 'fade-in' : ''} 
+            relative z-10`}
+        >
+            <div className="static h-screen w-screen">
+                <div className="
+                    absolute
+                    top-[20%] left-[10%]
+                    text-2xl text-white
+                    sm:top-[20%] sm:left-[10%] sm:text-3xl 
+                    md:top-[20%] md:left-[13%] md:text-4xl 
+                    lg:top-[20%] lg:left-[15%] lg:text-6xl
+                    font-SUITERegular" style={{ lineHeight: '1.4' }}
+                >
+                    근골격계의<br />단 하나의 목적, <br />
+                </div>
+                <div className="
+                    absolute
+                    top-[33%] left-[10%]
+                    text-xs leading-relaxed
+                    sm:top-[36%] sm:left-[10%] sm:text-xs
+                    md:top-[22%] md:left-[40%] md:text-sm
+                    lg:top-[23%] lg:left-[45%] lg:text-base"
+                >
+                    {`에베소서 5:29-30                
+                    누구든지 언제나 자기 육체를 미워하지 않고 오직 양육하여 보호하기를
+                    그리스도께서 교회에게 함과 같이 하나니 우리는 그 몸의 지체임이라 
+                    
+                    잠언 17:22
+                    마음의 즐거움은 양약이라도
+                    심령의 근심은 뼈를 마르게 하느니라 
+                    
+                    호세아 6:1
+                    오라 우리가 여호와께로 돌아가자
+                    여호와께서 우리를 찢으셨으나 도로 낫게 하실것이요
+                    우리를 치셨으나 싸매어 주실것임이라.
+                    
+                    잠언 3:1-2
+                    내 아들아 나의 법을 잊어버리지 말고 네 마음으로 나의 명령을 지키라
+                    그리하면 그것이 네가 장수하여 많은 해를 누리게하며 평강을 더하게 하리라`
+                    }
+                </div>
+                <div className="
+                    absolute
+                    bottom-[20%] left-[10%]
+                    text-2xl text-white
+                    sm:bottom-[20%] sm:right-[20%] sm:text-4xl
+                    md:bottom-[20%] md:right-[20%] md:text-5xl
+                    lg:bottom-[20%] lg:right-[10%] lg:text-6xl
+                    font-SUITERegular"
+                >
+                    “좋은 자세로 잘 움직이는 것”
+                </div>
             </div>
-            <div className="pl-144 text-left text-base leading-relaxed">
-                {`
-                에베소서 5:29-30                
-                누구든지 언제나 자기 육체를 미워하지 않고 오직 양육하여 보호하기를
-                그리스도께서 교회에게 함과 같이 하나니 우리는 그 몸의 지체임이라 
-                
-                잠언 17:22
-                마음의 즐거움은 양약이라도
-                심령의 근심은 뼈를 마르게 하느니라 
-                
-                호세아 6:1
-                오라 우리가 여호와께로 돌아가자
-                여호와께서 우리를 찢으셨으나 도로 낫게 하실것이요
-                우리를 치셨으나 싸매어 주실것임이라.
-                
-                잠언 3:1-2
-                내 아들아 나의 법을 잊어버리지 말고 네 마음으로 나의 명령을 지키라
-                그리하면 그것이 네가 장수하여 많은 해를 누리게하며 평강을 더하게 하리라
-                `}
-            </div>
-            <div className="absolute bottom-40 right-40 p-4 text-white text-6xl font-SUITERegular"style={{ lineHeight: '1.4' }}>
-                “좋은 자세로 잘 움직이는 것”
-            </div>
-        </div>
-    </section>
+        </section>
     );
 }
